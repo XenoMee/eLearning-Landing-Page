1df8d43480d65b43b39737030f044b43c6c0c898
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-18 16:48:08 +0200

    create enable moon icon function

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let storagedDarkMode = localStorage.getItem("darkmode");
 const moonIcon = darkModeToggleButton.children[0];
 const sunIcon = darkModeToggleButton.children[1];
 
-// function enableMoonIcon(){
-
-// }
+function enableMoonIcon() {
+  moonIcon.style.display = "inline-block";
+  sunIcon.style.display = "none";
+}
 
 // darkModeToggleButton.addEventListener("click", function () {
 //     if(storagedDarkMode !== "enabled") {
