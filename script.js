aff22ae26e31e0064f76a0ca463e435d220c0e74
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-18 16:47:02 +0200

    get hold of moon and sun icons of toggle button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const darkModeToggleButton = document.getElementById("toggle-button");
+let storagedDarkMode = localStorage.getItem("darkmode");
+const moonIcon = darkModeToggleButton.children[0];
+const sunIcon = darkModeToggleButton.children[1];
+
+// function enableMoonIcon(){
+
+// }
+
+// darkModeToggleButton.addEventListener("click", function () {
+//     if(storagedDarkMode !== "enabled") {
+
+//     }
+// });
